14dde61683bf2d6ba30e9e4edd61fedc0579e8df
myparamita <myparamita@gmail.com>
2017-01-20 21:14:34 +0800

    first commit

---
+++ src/Chapter02/HelloWorld.java
@@ -0,0 +1,16 @@
+package Chapter02;
+
+/***
+ * 
+ * @author niushuai
+ * 
+ *         1. 用javac编译后在当前目录生成HelloWorld.class  javac HelloWorld.java
+ *         2.用java执行的时候，需要包括Chapter01包。所以需要在Chapter02的上一层目录进行
+ *         java Chapter02.HelloWorld
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
